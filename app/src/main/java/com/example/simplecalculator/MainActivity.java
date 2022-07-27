10b535aebd631b81052072d15461f836bddbb7d0
Disha152 <disha29082001@gmail.com>
2022-07-27 05:56:30 +0530

    This is my first simple calculator app made with android studio

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.simplecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Add(View v){
+        EditText et1= (EditText)findViewById(R.id.editTextNumber);
+        EditText et2= (EditText)findViewById(R.id.editTextNumber2);
+        EditText et3= (EditText)findViewById(R.id.editTextNumber3);
+
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result = n1+n2;
+        et3.setText("Your Answer is ="+result);
+    }
+    public void Subtract(View v){
+        EditText et1= (EditText)findViewById(R.id.editTextNumber);
+        EditText et2= (EditText)findViewById(R.id.editTextNumber2);
+        EditText et3= (EditText)findViewById(R.id.editTextNumber3);
+
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result = n1-n2;
+        et3.setText("Your Answer is ="+result);
+    }
+    public void Multiply(View v){
+        EditText et1= (EditText)findViewById(R.id.editTextNumber);
+        EditText et2= (EditText)findViewById(R.id.editTextNumber2);
+        EditText et3= (EditText)findViewById(R.id.editTextNumber3);
+
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result = n1*n2;
+        et3.setText("Your Answer is ="+result);
+    }
+    public void Divide(View v){
+        EditText et1= (EditText)findViewById(R.id.editTextNumber);
+        EditText et2= (EditText)findViewById(R.id.editTextNumber2);
+        EditText et3= (EditText)findViewById(R.id.editTextNumber3);
+
+        int n1=Integer.parseInt(et1.getText().toString());
+        int n2=Integer.parseInt(et2.getText().toString());
+        int result = n1/n2;
+        et3.setText("Your Answer is ="+result);
+    }
+
+}
